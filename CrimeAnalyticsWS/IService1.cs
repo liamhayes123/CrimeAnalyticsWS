3304d2f462ae2ec3ca1d598ed145f3f319c3d6e3
liamhayes123 <liam_hayes123@hotmail.co.uk>
2015-01-10 19:33:01 +0000

    initial commit

---
+++ CrimeAnalyticsWS/IService1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CrimeAnalyticsWS
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "getData/{value}")]
+        string GetData(string value);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "GetAllCrimes")]
+        List<wsCrime> GetAllCrimes();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "GetAllCrimesInBoundary/?Coordinates={Coordinates}")]
+        List<wsCrime> GetAllCrimesInBoundary(string Coordinates);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "GetAllCrimesInBoundaryByCategory/?Coordinates={Coordinates}&selectedCategories={selectedCategories}")]
+        List<wsCrime> GetAllCrimesInBoundaryByCategory(string Coordinates, string selectedCategories);
+    }
+}
---
+++ CrimeAnalyticsWS/Service1.svc.cs
@@ -0,0 +1,65 @@
+﻿using CrimeAnalytics;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace CrimeAnalyticsWS
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(string value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public List<wsCrime> GetAllCrimes()
+        {
+            List<wsCrime> results = new List<wsCrime>();
+            List<Crime> dbCrimes = DatabaseCalls.GetAllCrimes();
+            foreach (Crime dbCrime in dbCrimes)
+            {
+                results.Add(new wsCrime(dbCrime));
+            }
+
+            return results;
+        }
+
+        public List<wsCrime> GetAllCrimesInBoundary(string coordinates)
+        {
+            List<wsCrime> results = new List<wsCrime>();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            List<Coordinates> coords = js.Deserialize<List<Coordinates>>(coordinates);
+            List<Crime> dbCrimes = DatabaseCalls.GetAllCrimesInBoundary(coords);
+            foreach (Crime dbCrime in dbCrimes)
+            {
+                results.Add(new wsCrime(dbCrime));
+            }
+            return results;
+        }
+
+        public List<wsCrime> GetAllCrimesInBoundaryByCategory(string coordinates, string selectedCategories)
+        {
+            string[] categoriesArray = JsonConvert.DeserializeObject<string[]>(selectedCategories);
+            string categories = string.Join(",", categoriesArray);
+            List<wsCrime> results = new List<wsCrime>();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            List<Coordinates> coords = js.Deserialize<List<Coordinates>>(coordinates);
+            List<Crime> dbCrimes = DatabaseCalls.GetAllCrimesInBoundaryByCategory(coords, categories);
+            foreach (Crime dbCrime in dbCrimes)
+            {
+                results.Add(new wsCrime(dbCrime));
+            }
+            return results;
+        }
+
+    }
+}
